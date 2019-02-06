d646476df41d2f6606fc79e85b7eb2a73fe71eab
hhalterman <hhalter@bgsu.edu>
2019-02-06 17:02:39 -0500

    working app

---
+++ app/data/friends.js
@@ -0,0 +1,38 @@
+// Templated Friends Data
+var friendsData = [
+  {
+    name: "Hailey Halterman",
+    photo: "https://thenypost.files.wordpress.com/2018/05/180516-woman-mauled-by-angry-wiener-dogs-feature.jpg?quality=90&strip=all&w=618&h=410&crop=1https://avatars0.githubusercontent.com/u/43506564?s=460&v=4",
+    scores: [1, 3, 2, 4, 3, 4, 1, 5, 4, 3]
+  },
+  {
+    name: "Norman Bates",
+    photo:"https://https://upload.wikimedia.org/wikipedia/en/thumb/c/cf/Norman_Bates_in_%22Psycho%22_%281960%29.jpg/250px-Norman_Bates_in_%22Psycho%22_%281960%29.jpg.wikimedia.org/wikipedia/en/thumb/e/e5/No_2_APTSWSM.jpg/200px-No_2_APTSWSM.jpg",
+    scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+  },
+  {
+    name: "Trinity",
+    photo:"http://vignette3.wikia.nocookie.net/matrix/images/6/67/Trinityfull.jpg/revision/latest?cb=20060803214449",
+    scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+  },
+  {
+    name: "Spider-man",
+    photo:"https://pmcvariety.files.wordpress.com/2017/02/spidermanavengersdowney.jpg?w=1000&h=563&crop=1",
+    scores: [5, 4, 4, 4, 4, 3, 2, 1, 5, 5]
+  },
+  {
+    name: "Sabrina Spellman",
+    photo:"https://www.thewrap.com/wp-content/uploads/2018/10/Chilling-Adventures-of-Sabrina-3.jpg",
+    scores: [2, 4, 2, 4, 2, 3, 4, 4, 4, 3]
+  },
+  {
+    name: "Princess Leia",
+    photo:"http://images.techtimes.com/data/images/full/168082/princess-leia-organa.jpg?w=600",
+    scores: [5, 4, 5, 4, 5, 3, 3, 3, 2, 4]
+  }
+];
+
+
+
+// Export to API Route
+module.exports = friendsData;
