2584a6a18e754222fa661ab7b80eea9bcb8cdb7b
hhalterman <hhalter@bgsu.edu>
2019-02-07 17:49:43 -0500

    quick addition

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -13,6 +13,10 @@ function htmlRoutes(app) {
     res.sendFile(path.join(__dirname + '/../public/survey.html'));
   });
 
+  app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/../public/home.html'));
+  });
+
   // A default USE route that leads to home.html which displays the home page.
   app.use(function (req, res) {
     res.sendFile(path.join(__dirname + '/../public/home.html'));
